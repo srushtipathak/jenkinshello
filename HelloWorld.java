e9ac42ce7baa2ad1b9066af0aee7629061a71af5
srushtipathak <srushtipathak16@gmail.com>
2020-11-21 13:25:16 +0530

    updates

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld{
 
 public static void main(String args[]){
 
-System.out.println("Hello guysssss... This is Srush..Yoo");
+System.out.println("Hello guysssss... This is Srush");
 
 }}
